44603f062579cdf3f308820029678a0a16ba3749
Moeka Nakane <mnak534@aucklanduni.ac.nz>
2022-05-14 20:52:54 +1200

    Changed the method determining dealer's action according to the piazza clarification

--- src/main/java/nz/ac/auckland/se281/a3/dealer/DealerStrategy.java
+++ src/main/java/nz/ac/auckland/se281/a3/dealer/DealerStrategy.java
@@ -18,16 +18,37 @@ public abstract class DealerStrategy {
 		decideTarget();
 		System.out.println("The dealer's target: " + this.target.getName());
 		Hand targetHand = target.getHand();
+
+		// Dealer's action to defeat the target
+
+		// If the target is busted, the dealer wins anyway, so HOLD
 		if (targetHand.isBust()) {
 			return Action.HOLD;
-		} else if (targetHand.isBlackJack() && !dealerHand.isBlackJack()) {
+		}
+
+		// If the target did blackjack, but the dealer did not,
+		// Then it's impossible for the dealer to defeat the target.
+		else if (targetHand.isBlackJack() && !dealerHand.isBlackJack()) {
+			// In such a case, if the dealer's score is >= 17, the dealer HOLD
 			if (dealerHand.getScore() >= 17) {
 				return Action.HOLD;
-			} else {
+			}
+			// Otherwise, HIT
+			else {
 				return Action.HIT;
 			}
-		} else if (targetHand.getScore() > dealerHand.getScore() && !dealerHand.isBust()) {
-			return Action.HIT;
+		}
+
+		// If the target's score is <= 21 AND is not blackjack
+		else if (targetHand.getScore() <= 21 && !targetHand.isBlackJack()) {
+			// The dealer HIT if the dealer's score < the target's score
+			if (dealerHand.getScore() < targetHand.getScore()) {
+				return Action.HIT;
+			}
+			// The dealer HOLD otherwise (already winning)
+			else {
+				return Action.HOLD;
+			}
 		}
 		return Action.HOLD;
 	}
